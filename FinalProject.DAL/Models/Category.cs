51fc71a3280cfb7b59247e439f4febc05a701cdd
Kazzie Spevacek <kas7680@psu.edu>
2023-11-28 17:43:18 -0500

    work on project

--- FinalProject.DAL/Models/Category.cs
+++ FinalProject.DAL/Models/Category.cs
@@ -5,5 +5,12 @@ namespace FinalProject.DAL.Models
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
+        public string CategoryImage
+        {
+            get
+            {
+                return $"{CategoryId}.png";
+            }
+        }
     }
 }
